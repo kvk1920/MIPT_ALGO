00d6b44fd7c583541aaf4ba93d9aaa1999b31b25
kvk1920 <34111966+kvk1920@users.noreply.github.com>
2018-10-23 13:41:23 +0300

    Update FlowNetwork.cpp

--- Reviews/FlowNetwork.cpp
+++ Reviews/FlowNetwork.cpp
@@ -38,6 +38,8 @@ private:
 public:
     FlowNetwork(size_t n);
 
+    void insertEdge(id_t fromVertex, id_t toVertex, capacity_t capacity, bool is_directed = true);
+
     auto edges() const;
     auto edges();
 
@@ -45,8 +47,10 @@ public:
     class edge_iterator : public std::iterator<std::forward_iterator_tag, T>
     {
     private:
-        id_t            edgeId_;
+        id_t            edgeId_ = None;
+        FlowNetwork<capacity_t>* network_;
     public:
+        friend class VertexView;
         T* operator->() const;
         T& operator*() const;
         auto& operator++();
@@ -58,6 +62,7 @@ public:
     {
     private:
         id_t vertexId_ = None;
+        FlowNetwork<capacity_t>* network_;
     public:
         auto begin() const;
         auto end() const;
@@ -126,4 +131,93 @@ template <typename capacity_t> inline
 auto FlowNetwork<capacity_t>::edges()
 {
     return ContainerView<std::vector<edge_t>>(&edges_);
+}
+
+
+template <typename capacity_t>
+template <typename T>
+inline T&
+FlowNetwork<capacity_t>::edge_iterator<T>::operator*() const
+{
+    return network_->edges_[edgeId_];
+}
+
+template <typename capacity_t>
+template <typename T>
+inline T*
+FlowNetwork<capacity_t>::edge_iterator<T>::operator->() const
+{
+    return &network_->edges_[edgeId_];
+}
+
+template <typename capacity_t>
+template <typename T>
+inline auto&
+FlowNetwork<capacity_t>::edge_iterator<T>::operator++()
+{
+    edgeId_ = network_->previous_edge_[edgeId_];
+    return *this;
+}
+
+template <typename capacity_t>
+template <typename T>
+inline auto
+FlowNetwork<capacity_t>::edge_iterator<T>::operator++(int)
+{
+    auto result(*this);
+    operator++();
+    return result;
+}
+
+template <typename capacity_t>
+template <bool is_const>
+inline auto
+FlowNetwork<capacity_t>::VertexView<is_const>::begin() const
+{
+    if constexpr (is_const)
+    {
+        return edge_iterator<const edge_t>{network_->last_edge_[vertexId_], network_};
+    }
+    else
+    {
+        return edge_iterator<edge_t>{network_->last_edge_[vertexId_], network_};
+    }
+}
+
+template <typename capacity_t>
+template <bool is_const>
+inline auto
+FlowNetwork<capacity_t>::VertexView<is_const>::end() const
+{
+    if constexpr (is_const)
+    {
+        return edge_iterator<const edge_t>{None, network_};
+    }
+    else
+    {
+        return edge_iterator<edge_t>{None, network_};
+    }
+}
+
+template <typename capacity_t> inline auto
+FlowNetwork<capacity_t>::vertex(id_t vertexId) const
+{
+    return VertexView<true>{vertexId, this};
+}
+
+template <typename capacity_t> inline auto
+FlowNetwork<capacity_t>::vertex(id_t vertexId)
+{
+    return VertexView<false>{vertexId, this};
+}
+
+template <typename capacity_t>
+inline void FlowNetwork<capacity_t>::insertEdge(
+        id_t fromVertex,
+        id_t toVertex,
+        capacity_t capacity,
+        bool is_directed)
+{
+    baseInsertEdge(fromVertex, toVertex, capacity);
+    baseInsertEdge(toVertex, fromVertex, is_directed ? capacity_t(0) : capacity);
 }

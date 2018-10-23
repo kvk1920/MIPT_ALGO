a6edd4f4b7bbdb08bb4bc1c0471dab706b7e693e
kvk1920 <34111966+kvk1920@users.noreply.github.com>
2018-10-23 12:50:18 +0300

    Update FlowNetwork.cpp

--- Reviews/FlowNetwork.cpp
+++ Reviews/FlowNetwork.cpp
@@ -1,5 +1,18 @@
 #include <vector>
 
+template <class Container>
+struct ContainerView
+{
+private:
+    Container*    container_;
+public:
+    ContainerView(Container* container);
+    auto begin() const;
+    auto end() const;
+    auto begin();
+    auto end();
+};
+
 template <typename capacity_t>
 class FlowNetwork
 {
@@ -24,7 +37,62 @@ private:
     void baseInsertEdge(id_t fromVertex, id_t toVertex, capacity_t capacity);
 public:
     FlowNetwork(size_t n);
+
+    auto edges() const;
+    auto edges();
+
+    template <typename T>
+    class edge_iterator : public std::iterator<std::forward_iterator_tag, T>
+    {
+    private:
+        id_t            edgeId_;
+    public:
+        T* operator->() const;
+        T& operator*() const;
+        auto& operator++();
+        auto operator++(int);
+    };
+
+    template <bool is_const = false>
+    struct VertexView
+    {
+    private:
+        id_t vertexId_ = None;
+    public:
+        auto begin() const;
+        auto end() const;
+    };
+
+     auto vertex(id_t vertexId) const;
+     auto vertex(id_t vertexId);
 };
+
+template <class Container> inline
+ContainerView<Container>::ContainerView(Container* container) : container_(container) {}
+
+template <class Container> inline
+auto ContainerView<Container>::begin()
+{
+    return container_->begin();
+}
+
+template <class Container> inline
+auto ContainerView<Container>::end()
+{
+    return container_->end();
+}
+
+template <class Container> inline
+auto ContainerView<Container>::begin() const
+{
+    return container_->begin();
+}
+
+template <class Container> inline
+auto ContainerView<Container>::end() const
+{
+    return container_->end();
+}
 
 template <typename capacity_t> inline capacity_t
 FlowNetwork<capacity_t>::edge_t::residualCapacity() const
@@ -47,3 +115,15 @@ template <typename capacity_t> inline
 FlowNetwork<capacity_t>::FlowNetwork(size_t n)
     : last_edge_(n, None)
 {}
+
+template <typename capacity_t> inline
+auto FlowNetwork<capacity_t>::edges() const
+{
+    return ContainerView<const std::vector<edge_t>>(&edges_);
+}
+
+template <typename capacity_t> inline
+auto FlowNetwork<capacity_t>::edges()
+{
+    return ContainerView<std::vector<edge_t>>(&edges_);
+}

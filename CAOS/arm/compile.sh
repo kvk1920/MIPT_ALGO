9eb0a298923a3caa128ebb2d8492b54a52c89e02
kvk1920 <kvk1901@yandex.ru>
2018-10-30 13:01:40 +0300

    Added some files

---
+++ CAOS/arm/compile.sh
@@ -0,0 +1,2 @@
+arm-gcc/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc -marm $*
+
---
+++ CAOS/arm/run.sh
@@ -0,0 +1,1 @@
+qemu-arm -L arm-sysroot/sysroot-glibc-linaro-2.25-2018.05-arm-linux-gnueabi/ $*
